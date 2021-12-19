d1333882e23b9efcb3a9baa5d541c933bd6fc676
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-12-19 03:26:27 +0100

    Save data

--- R/users.R
+++ R/users.R
@@ -23,7 +23,10 @@ users_raw <- get_followers("psicotuiterbot", token = my_token, n = 14000)
 
 saveRDS(users_raw, here("data", "followers-bot.rds"))
 
-# get top followers (by N followers)
+
+# get top N followers with most followers
+N <- 10
+
 top_accounts <- users_raw %>% 
     distinct(user_id) %>%
     pull(user_id) %>% 
@@ -31,34 +34,24 @@ top_accounts <- users_raw %>%
     pull(screen_name) %>% 
     lookup_users(token = my_token) %>%
     users_data() %>% 
-    top_n(n = 10, wt = followers_count) %>% 
+    top_n(n = N, wt = followers_count) %>% 
     arrange(followers_count) %>% 
     pull(screen_name)
 
 saveRDS(top_accounts, here("data", "followers-bot.rds"))
 
-# get followers of followers
+# get followers of top followers
 followers <- map(top_accounts, get_followers, n = 14000, token = my_token) %>% 
     set_names(top_accounts) %>% 
     bind_rows(.id = "account")
 
-saveRDS(top_accounts, here("data", "top-followers.rds"))
+saveRDS(followers, here("data", "top-followers.rds"))
 
 count(followers, account) %>% 
     ggplot(aes(reorder(account, -n), n)) +
     geom_col()
 
-
-net <-  network_data(tweets, .e = c("mention, reply, quote"))
-
-
-plot(
-    graph, 
-    width = 0.1
-    arrow.size = 0.1,
-    arrow.width = 0.1,
-    shape = "square",
-    label.cex = 0.1,
-    color = "white",
-    label.color = "black"
-)
+# get common followers from the top N accounts
+common <- split(followers, followers$account) %>% 
+    map("user_id") %>% 
+    reduce(intersect)
